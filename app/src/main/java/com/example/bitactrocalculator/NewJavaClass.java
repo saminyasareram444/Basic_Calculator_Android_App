57c3f9a514c749ea917062c061be1fab54df129b
Mahfuzul Haque <mahfuz15.ca@gmail.com>
2021-11-15 14:21:09 +0600

    New java class added

---
+++ app/src/main/java/com/example/bitactrocalculator/NewJavaClass.java
@@ -0,0 +1,17 @@
+package com.example.bitactrocalculator;
+
+public class NewJavaClass {
+    int x;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public NewJavaClass() {
+        this.x = 6;
+    }
+}
